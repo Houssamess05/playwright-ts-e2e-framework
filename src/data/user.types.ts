aee082031f42889d94dca0897c238025188305e5
Houssamess05 <houssam.essarhiar@gmail.com>
2026-08-27 17:28:24 +0200

    Create DTO interfaces

--- src/data/user.types.ts
+++ src/data/user.types.ts
@@ -14,3 +14,24 @@ export interface UserData {
   zipcode: string;
   mobileNumber: string;
 }
+
+// Api response types
+export interface UserPayload {
+  name: string;
+  job: string;
+}
+
+export interface CreateUserResponse extends UserPayload {
+  id: string;
+  createdAt: string;
+}
+
+export interface UserDetailsResponse {
+  data: {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+  };
+}
